2da54180214e333256a5d15ed09bcf211244d1a0
wumusic <146039491+wumusic@users.noreply.github.com>
2023-10-26 12:11:13 +0800

    mlist.js

--- mlist.js
+++ mlist.js
@@ -23,12 +23,6 @@ var data = {
             "audio": "4.mp3",
             "name": "下雨的时候大家都在干嘛"
         }
-        ,
-        {
-
-            "id": "5",
-            "audio": "https://sharefs.ali.kugou.com/202310261152/41ea4dea8bb189d89a3d62d67e4b2dd3/v3/47bca5f9306d1c17b8d1365edec8a0e5/yp/full/a1000_u0_p409_mx391060381_s377713591.mp3",
-            "name": "叛变神曲"
-        }
+       
     ]
 }
